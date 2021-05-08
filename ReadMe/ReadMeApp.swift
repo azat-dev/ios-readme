22bb53bc9208d73ca3e3d7ff96e2676d41f328cf
Azat <kaumov.a.r@gmail.com>
2021-05-08 21:52:51 +0300

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Azat Kaiumov on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
